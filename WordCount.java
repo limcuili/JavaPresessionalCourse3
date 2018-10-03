7ed310bcdf9c70ab8e387247ee3a44d932e4016c
limcuili <limcuili@hotmail.com>
2018-10-03 12:05:14 +0100

    improved WordCount to take into account a string of whitespace between words

--- WordCount.java
+++ WordCount.java
@@ -9,25 +9,30 @@ class WordCount {
     // task: counts the number of words of a text that is taken in as a user input.
     // a word is defined to not contain whitespaces, and have whitespace(s) preceding and following it.
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         if (args.length < 1) {
             return;
         }
         String text = args[0];
         int wordCount = 0;
-        for (int charPlace = 0; charPlace < text.length(); charPlace++) {
-            if (Character.isWhitespace( text.charAt(charPlace) )) {
-                wordCount++;
+        boolean prevCharIsWhitespace = false;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isWhitespace(text.charAt(i))) {
+                prevCharIsWhitespace = true;
+            } else {
+                if (prevCharIsWhitespace == true) {wordCount++;}
+                prevCharIsWhitespace = false;
             }
         }
-        System.out.println("word count = " + (wordCount += 1));
+        System.out.println("word count = " + (wordCount + 1));
     }
 
 }
 
 /*
-The above is a simple word count that has to follow english grammar conventions because it counts spaces.
-eg. java WordCount "Hello" >> word count = 1
-eg. java WordCount "Hello person" >> word count = 2
-eg. java WordCount "Hello " >> word count = 2
+eg. java WordCount "Hello world" >> word count = 2
+eg. java WordCount "Hello world ?" >> word count = 3
+eg. java WordCount "Hello world ? " >> word count = 3
+HOWEVER eg. java WordCount " Hello world ? " >> word count = 4 (note the leading space)
+ALSO, this program does not take into account an empty string.
 */
