de1f226b826e5be0574990759fbcfe19a8a13a0e
limcuili <limcuili@hotmail.com>
2018-10-03 16:37:32 +0100

    Completed code

--- WordCount.java
+++ WordCount.java
@@ -1,22 +1,38 @@
 /*
-HOMEWORK to be handed in via moodle by Thu 4 Oct to Dr Konrad Swanepoel
 name: Cui-Li LIM
 student number: 201813956
 */
 
 class WordCount {
 
-    // task: counts the number of words of a text that is taken in as a user input.
-    // a word is defined to not contain whitespaces, and have whitespace(s) preceding and following it.
+    /*
+    this program counts the number of words of a text that is taken in as an argument.
+    eg. java WordCount "    " >>> word count = 0
+    eg. java WordCount "Hello world" >>> word count = 2
+    eg. java WordCount "Hello world ?!" >>> word count = 3
+    eg. java WordCount "  Hello   world  ?  " >>> word count = 3
+    */
 
     public static void main(String[] args) {
         if (args.length < 1) {
             return;
         }
         String text = args[0];
-        int wordCount = 0;
+        boolean stringIsEmpty = true;
+        for (int j = 0; j < text.length(); j++) {
+            if (text.charAt(j) != ' ') {
+                stringIsEmpty = false;
+            }
+        }
+
+        int leadingChar = 0;
+        for (int i = 0; i < text.length() && text.charAt(i) == ' '; i++) {
+            leadingChar++;
+        }
+
+        int wordCount = 1;
         boolean prevCharIsWhitespace = false;
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = leadingChar; i < text.length(); i++) {
             if (Character.isWhitespace(text.charAt(i))) {
                 prevCharIsWhitespace = true;
             } else {
@@ -25,27 +41,12 @@ class WordCount {
             }
         }
 
-        boolean allIsWhitespace = true;
-        for (int j = 0; j < text.length(); j++) {
-            if (text.charAt(j) != ' ') {
-                allIsWhitespace = false;
-            }
-        }
-
-        if (allIsWhitespace) {
+        if (stringIsEmpty) {
             System.out.println("word count = 0");
         } else {
-            System.out.println("word count = " + (wordCount + 1));
+            System.out.println("word count = " + (wordCount));
         }
 
     }
 
 }
-
-/*
-eg. java WordCount "    " >> word count = 0
-eg. java WordCount "Hello world" >> word count = 2
-eg. java WordCount "Hello world ?" >> word count = 3
-eg. java WordCount "Hello world ? " >> word count = 3
-HOWEVER eg. java WordCount " Hello world ? " >> word count = 4 (note the leading space)
-*/
